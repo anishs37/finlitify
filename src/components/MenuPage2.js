79fff19231a86c6e935ac3eb8270257a7b400091
SnazzyBeatle115 <64025519+SnazzyBeatle115@users.noreply.github.com>
2023-12-03 17:54:57 -0500

    pretty

--- src/components/MenuPage2.js
+++ src/components/MenuPage2.js
@@ -1,33 +1,12 @@
 // MenuPage2.js
 
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Footer from './Footer';
-import axios from 'axios';
 import './MenuPage2.css';
 import './HomePage.css';
 
-const MenuPage2 = ({ wolframApiKey }) => {
-  const [question, setQuestion] = useState('');
-  const [answer, setAnswer] = useState('');
-
-  const handleAskQuestion = async () => {
-    try {
-      const encodedQuery = encodeURIComponent(question);
-      const apiKey = 'HHYEJK-4W4E445VVH'; // Replace with your actual Wolfram Alpha API key
-      const apiUrl = `https://cors-anywhere.herokuapp.com/http://api.wolframalpha.com/v2/query?appid=${apiKey}&input=${encodedQuery}&output=json`;
-  
-      const response = await axios.get(apiUrl);
-  
-      const pods = response.data.queryresult.pods;
-      const primaryPod = pods[1];
-      const primaryAnswer = primaryPod && primaryPod.subpods[0].plaintext;
-  
-      setAnswer(primaryAnswer || 'No answer available');
-    } catch (error) {
-      console.error('Error fetching data from Wolfram Alpha:', error);
-    }
-  };
+const MenuPage2 = ({}) => {
   
 
   return (
@@ -52,16 +31,7 @@ const MenuPage2 = ({ wolframApiKey }) => {
           Saving Strategies
         </Link>
       </div>
-      <div className="wolfram-section">
-        <input
-          type="text"
-          placeholder="Ask a question"
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
-        />
-        <button onClick={handleAskQuestion}>Ask</button>
-        <div className="answer">{answer}</div>
-      </div>
+      
       <Footer />
     </div>
   );
