460a217d26288a46656207407034e7370ee4f581
SnazzyBeatle115 <64025519+SnazzyBeatle115@users.noreply.github.com>
2023-12-03 19:39:18 -0500

    wawa new master

---
+++ src/components/budgetQuestions.js
@@ -0,0 +1,148 @@
+const budgetQuestions = {
+  "What is the primary purpose of a budget?": {
+    level: 1,
+    choices: ["To spend money randomly", "To plan and manage money", "To avoid financial goals"],
+    correctAnswer: "To plan and manage money"
+  },
+  "How often should you review your budget?": {
+    level: 1,
+    choices: ["Once a week", "Once a month", "Once a year"],
+    correctAnswer: "Once a month"
+  },
+  "What are fixed expenses in a budget?": {
+    level: 1,
+    choices: ["Expenses that change regularly", "Expenses that stay constant", "Optional expenses"],
+    correctAnswer: "Expenses that stay constant"
+  },
+  "Why is it important to track expenses in a budget?": {
+    level: 1,
+    choices: ["It has no impact on budgeting", "To identify spending patterns", "To increase debt"],
+    correctAnswer: "To identify spending patterns"
+  },
+  "Which of the following is an example of a variable expense?": {
+    level: 1,
+    choices: ["Rent/Mortgage", "Groceries", "Internet bill"],
+    correctAnswer: "Groceries"
+  },
+  "What does the 50/30/20 rule in budgeting suggest?": {
+    level: 1,
+    choices: [
+      "Spend 50% on wants, 30% on needs, 20% on savings",
+      "Spend 50% on needs, 30% on wants, 20% on savings",
+      "Spend 50% on savings, 30% on needs, 20% on wants"
+    ],
+    correctAnswer: "Spend 50% on needs, 30% on wants, 20% on savings"
+  },
+  "Why should you have an emergency fund in your budget?": {
+    level: 1,
+    choices: ["To fund planned vacations", "To cover unexpected expenses", "To invest in the stock market"],
+    correctAnswer: "To cover unexpected expenses"
+  },
+  "What is discretionary income?": {
+    level: 1,
+    choices: ["Money set aside for emergencies", "Money available after essential expenses", "Money spent on non-essential items"],
+    correctAnswer: "Money available after essential expenses"
+  },
+  "How does budgeting help in avoiding debt?": {
+    level: 1,
+    choices: ["By borrowing as much as possible", "By managing expenses within income limits", "By ignoring a budget altogether"],
+    correctAnswer: "By managing expenses within income limits"
+  },
+  "What is the purpose of the sinking fund in a budget?": {
+    level: 1,
+    choices: ["To save for a vacation", "To fund planned future expenses", "To pay off debt"],
+    correctAnswer: "To fund planned future expenses"
+  },
+  "What is the key difference between needs and wants in budgeting?": {
+    level: 2,
+    choices: ["Needs are unnecessary, wants are essential", "Needs are essential, wants are unnecessary", "Needs and wants are the same"],
+    correctAnswer: "Needs are essential, wants are unnecessary"
+  },
+  "How can you prioritize spending in a budget effectively?": {
+    level: 2,
+    choices: ["By spending randomly", "By allocating money based on importance", "By ignoring priorities"],
+    correctAnswer: "By allocating money based on importance"
+  },
+  "In budgeting, what does zero-based budgeting involve?": {
+    level: 2,
+    choices: ["Spending without a plan", "Allocating every dollar to a category", "Ignoring budget categories"],
+    correctAnswer: "Allocating every dollar to a category"
+  },
+  "Why is tracking irregular income important in budgeting?": {
+    level: 2,
+    choices: ["Irregular income has no impact on budgeting", "Inconsistent cash flow can make budgeting challenging", "Budgeting is only for those with steady income"],
+    correctAnswer: "Inconsistent cash flow can make budgeting challenging"
+  },
+  "What role does a contingency fund play in a budget?": {
+    level: 2,
+    choices: ["A fund for luxury items", "A fund for planned vacations", "A fund for unexpected expenses"],
+    correctAnswer: "A fund for unexpected expenses"
+  },
+  "How does inflation impact budgeting over time?": {
+    level: 2,
+    choices: ["Reduces the need for budgeting", "Increases the cost of goods and services", "Has no effect on budgeting"],
+    correctAnswer: "Increases the cost of goods and services"
+  },
+  "What is the concept of discretionary income in budgeting?": {
+    level: 2,
+    choices: ["Money set aside for emergencies", "Money available after essential expenses", "Money spent on non-essential items"],
+    correctAnswer: "Money available after essential expenses"
+  },
+  "How can technology assist in budget tracking?": {
+    level: 2,
+    choices: ["By using a manual ledger", "By automating expense tracking", "By avoiding technology altogether"],
+    correctAnswer: "By automating expense tracking"
+  },
+  "Explain the concept of the opportunity cost in budget decision-making.": {
+    level: 2,
+    choices: ["The cost of missed opportunities", "The cost of making budget decisions", "Ignoring budget decisions"],
+    correctAnswer: "The cost of missed opportunities"
+  },
+  "What is the importance of improved financial communication in a family budget?": {
+    level: 2,
+    choices: ["It leads to increased debt", "It helps to avoid financial misunderstandings", "It has no impact on budgeting"],
+    correctAnswer: "It helps to avoid financial misunderstandings"
+  },
+  "Compare and contrast traditional budgeting with zero-based budgeting.": {
+    level: 3,
+    choices: ["Traditional budgeting is unnecessary", "Zero-based budgeting allocates every dollar", "Both are the same"],
+    correctAnswer: "Zero-based budgeting allocates every dollar"
+  },
+  "How can tax planning be integrated into a budget?": {
+    level: 3,
+    choices: ["By avoiding taxes altogether", "By considering tax implications in financial planning", "Taxes have no impact on budgeting"],
+    correctAnswer: "By considering tax implications in financial planning"
+  },
+  "Discuss the challenges of budgeting for irregular income.": {
+    level: 3,
+    choices: ["Irregular income has no impact on budgeting", "Inconsistent cash flow can make budgeting challenging", "Budgeting is only for those with steady income"],
+    correctAnswer: "Inconsistent cash flow can make budgeting challenging"
+  },
+  "What is the concept of a reverse budget?": {
+    level: 3,
+    choices: ["Allocating money in reverse order of importance", "Spending without a plan", "Ignoring budget categories"],
+    correctAnswer: "Allocating money in reverse order of importance"
+  },
+  "Why is a rolling budget useful in certain financial situations?": {
+    level: 3,
+    choices: ["A budget that stays fixed over time", "A budget that adapts to changing circumstances", "Rolling budgets have no relevance in financial planning"],
+    correctAnswer: "A budget that adapts to changing circumstances"
+  },
+  "How can budgeting be adapted for a freelance or self-employed individual?": {
+    level: 3,
+    choices: ["Freelancers don't need to budget", "By creating a flexible budget that accommodates variable income", "Budgeting is only for those with traditional employment"],
+    correctAnswer: "By creating a flexible budget that accommodates variable income"
+  },
+  "Discuss the role of financial ratios in evaluating budget health.": {
+    level: 3,
+    choices: ["Financial ratios are unrelated to budget health", "Financial ratios help evaluate financial performance", "Budget health is irrelevant in financial planning"],
+    correctAnswer: "Financial ratios help evaluate financial performance"
+  },
+  "Explain the concept of a sinking fund in budgeting.": {
+    level: 3,
+    choices: ["A fund for planned vacations", "A fund for unexpected expenses", "A fund for buying luxury items"],
+    correctAnswer: "A fund for planned vacations"
+  },
+};
+
+export default budgetQuestions;
---
+++ src/components/saveQuestions.js
@@ -0,0 +1,49 @@
+const savings_questions = {
+    "How would you define savings?": {
+        "level": 1,
+        "choices": ["a. Spending money on daily needs", "b. Putting money aside for future use", "c. Donating money to charity"],
+        "correctAnswer": "b. Putting money aside for future use"
+    },
+    "Why is it important to have a savings goal?": {
+        "level": 1,
+        "choices": ["a. Savings goals are unnecessary", "b. To have a clear purpose for saving", "c. Savings goals only benefit the rich"],
+        "correctAnswer": "b. To have a clear purpose for saving"
+    },
+    "What is the purpose of an emergency fund?": {
+        "level": 1,
+        "choices": ["a. To fund planned vacations", "b. To cover unexpected expenses", "c. To invest in the stock market"],
+        "correctAnswer": "b. To cover unexpected expenses"
+    },
+    "How can you automate your savings?": {
+        "level": 2,
+        "choices": ["a. By avoiding savings altogether", "b. By setting up automatic transfers", "c. By withdrawing cash regularly"],
+        "correctAnswer": "b. By setting up automatic transfers"
+    },
+    "Which of the following is a long-term savings goal?": {
+        "level": 2,
+        "choices": ["a. Buying a daily cup of coffee", "b. Purchasing a new smartphone", "c. Saving for retirement"],
+        "correctAnswer": "c. Saving for retirement"
+    },
+    "What is the concept of compound interest in savings?": {
+        "level": 2,
+        "choices": ["a. Earning interest only on the principal amount", "b. Earning interest on both the principal and accumulated interest", "c. Avoiding interest altogether"],
+        "correctAnswer": "b. Earning interest on both the principal and accumulated interest"
+    },
+    "How can you prioritize your savings goals effectively?": {
+        "level": 3,
+        "choices": ["a. By ignoring savings goals", "b. By allocating money based on importance", "c. By saving only for short-term goals"],
+        "correctAnswer": "b. By allocating money based on importance"
+    },
+    "Describe the concept of a Certificate of Deposit (CD) in savings.": {
+        "level": 3,
+        "choices": ["a. A tool for spending money", "b. A long-term savings account", "c. A time deposit with a fixed interest rate"],
+        "correctAnswer": "c. A time deposit with a fixed interest rate"
+    },
+    "Discuss the challenges of saving for a major financial goal like buying a home.": {
+        "level": 3,
+        "choices": ["a. Major financial goals are easy", "b. Major financial goals are impossible", "c. Major financial goals require careful planning and discipline"],
+        "correctAnswer": "c. Major financial goals require careful planning and discipline"
+    },
+};
+
+export default savings_questions;
